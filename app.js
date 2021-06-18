0fa87f433ab59251f415ab91d1af92de2292ef59
Soledad Vallejos <soledad_vallejos@hotmail.com>
2021-06-18 01:17:37 -0300

    se agrega la funcion extraccion

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ let acciones = require("./funciones");
 
 let process = require("process");
 let comando = process.argv[2].toLowerCase()
-let posicionTres = process.argv[3].toLowerCase();
+
+
+
 
 
 
 switch (comando) {
     case 'consultar':
+        let posicionTres = process.argv[3].toLowerCase();
         acciones.consulta(posicionTres);
         break;
-
+    case 'extraer':
+        let posicionCuatro = process.argv[4].toLowerCase();
+        acciones.extracion(posicionCuatro);
+        break;   
+        
     default:
 console.log("Seleccione una operacion");
         break;
 }
 
-console.log("hola");
+
--- funciones.js
+++ funciones.js
@@ -7,15 +7,11 @@ module.exports = acciones = {
         let listaDeTarea = fs.readFileSync('./cuentas.json', 'utf-8')
         let prueba = JSON.parse(listaDeTarea)
         return prueba
-            },
-     prueba: () =>{
-         let pruebas = acciones.leerJSON()
-         console.log(typeof(pruebas));
      },
     guardarJSON : (info) => {
         let nuevoJSON = JSON.stringify(info);
         fs.writeFileSync('./cuentas.json', nuevoJSON, 'utf-8')
-                    },
+     },
     consulta: (banco) => {
         let resultado = 0;
         let datos = acciones.leerJSON()
@@ -30,10 +26,10 @@ module.exports = acciones = {
             }
             break;
         case "ciudad":
-            if( datos[0].banco[0].nombre === "banco ciudad"){
+            if( datos[0].banco[0].nombre == "banco ciudad"){
                 resultado = datos[0].banco[0].monto;
                 console.log("El saldo de la cuenta Ciudad es : " + resultado);
-            }else if(datos[0].banco[1].nombre === "banco ciudad"){
+            }else if(datos[0].banco[1].nombre == "banco ciudad"){
                 resultado = datos[0].banco[1].monto;
                 console.log("El saldo de la cuenta Ciudad es : " + resultado);
             }
@@ -45,6 +41,38 @@ module.exports = acciones = {
     return resultado
     },
     extracion: (banco, monto) =>{
+        let resultado = 0;
+        let datos = acciones.leerJSON();
+        switch (banco) {
+            case "santander":
+                if (datos[0].banco[0].nombre === "santander"){
+                    resultado = datos[0].banco[0].monto -= monto //quiero que lo guarde en esta variable 
+                   console.log("Retiro la suma de: " + monto);//Muestre este resultado por consola
+                   resultado = acciones.guardarJSON;   
+                }else if(datos[0].banco[1].nombre === "santander"){
+                    resultado = datos[0].banco[1].monto -= monto;
+                    console.log("Retiro la suma de: " + monto);
+                    resultado = acciones.guardarJSON;
+                }
+                break;
+            case "cuidad":
+                if (datos[0].banco[0].nombre === "cuidad"){
+                    resultado = datos[0].banco[0].monto -= monto;  
+                   console.log("Retiro la suma de: " + monto);
+                   resultado = acciones.guardarJSON;   
+                }else if(datos[0].banco[1].nombre === "cuidad"){
+                    resultado = datos[0].banco[1].monto -= monto;
+                    console.log("Retiro la suma de: " + monto);
+                    resultado = acciones.guardarJSON;
+                }
+                break;
+            default: console.log("Debe ingresar un monto");
+                break;
+        }
+        return resultado
+    },
+    depositar: (banco, monto) =>{
 
-    }
+    } 
 }
+
